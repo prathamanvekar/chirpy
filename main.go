d56483304210d5aecdca153fc9ff35a284d3cf75
Prathamesh Anvekar <anvekarprathamesh13@gmail.com>
2026-06-08 21:54:20 +0530

    the server now servers static files using http.fileserver

--- main.go
+++ main.go
@@ -6,22 +6,17 @@ import (
 )
 
 func main() {
-	// Define the port we want our server to listen on
+	const filepathRoot = "."
 	const port = "8080"
 
-	// Create a new ServeMux (router) to handle different HTTP paths
 	mux := http.NewServeMux()
+	mux.Handle("/", http.FileServer(http.Dir(filepathRoot)))
 
-	// Initialize the server struct with the address and our router
 	srv := &http.Server{
 		Addr:    ":" + port,
 		Handler: mux,
 	}
 
-	// Print a message to the console so we know the server is starting
-	log.Printf("Serving on port: %s\n", port)
-
-	// Start the server and listen for incoming requests.
-	// If the server fails to start, log.Fatal will exit the program.
+	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
 	log.Fatal(srv.ListenAndServe())
 }
